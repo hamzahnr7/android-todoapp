9f442c0d7ff82b05386ea3169404efe2a032def9
strider15 <anas.satria.as@gmail.com>
2023-01-14 02:37:58 +0700

    Material design text input

--- src/screens/CreateTask.js
+++ src/screens/CreateTask.js
@@ -1,11 +1,14 @@
-import { Button, StyleSheet, Text, TextInput, View, ToastAndroid } from 'react-native'
+import { Button, StyleSheet, View, ToastAndroid } from 'react-native'
 import React, { useState } from 'react'
+import { TextInput, Text } from "@react-native-material/core";
+import DatePicker from 'react-native-date-picker'
 import axios from 'axios';
 import { url } from '../Env';
 
 export default function Todo({route, navigation}) {
   const params = route.params;
   const todoId = route.params.id; 
+  const [date, setDate] = useState(new Date())
 
   const [title, setTitle] = useState(params.title? params.title :'');
   const [desc, setDesc] = useState(params.description? params.description :'');
@@ -28,8 +31,8 @@ export default function Todo({route, navigation}) {
   const createTask = async (params) => {
     if (title == '' || desc == '' || status == '' || deadline == '') {
       return ToastAndroid.showWithGravity(
-        'All Your Base Are Belong To Us',
-        ToastAndroid.SHORT,
+        'Please fill all the blank form',
+        ToastAndroid.LONG,
         ToastAndroid.CENTER,
       )
     }
@@ -47,11 +50,13 @@ export default function Todo({route, navigation}) {
 
   return (
     <View>
-      <Text>Todo Details</Text>
-      <TextInput placeholder={'Type Title'} onChangeText={(text) => setTitle(text)} value={title}/>
-      <TextInput placeholder={'Fill Description'} onChangeText={(text) => setDesc(text)} value={desc}/>
-      <TextInput placeholder={'Set Status'} onChangeText={(text) => setStatus(text)} value={status}/>
-      <TextInput placeholder={'Set Deadline'} onChangeText={(text) => setDeadline(text)} value={deadline}/>
+      <Text variant="h4" style={styles.textWrapper} >
+      Create your new ToDo!
+      </Text>
+      <TextInput variant='outlined' label='Type Title' onChangeText={(text) => setTitle(text)} value={title}/>
+      <TextInput variant='outlined' label='Fill Description' onChangeText={(text) => setDesc(text)} value={desc}/>
+      <TextInput variant='outlined' label='Set Status' onChangeText={(text) => setStatus(text)} value={status}/>
+      <TextInput variant='outlined' label='Set Deadline' onChangeText={(text) => setDeadline(text)} value={deadline}/>
       <View style={{flexDirection: 'row', alignSelf: 'center'}}>
         <View style={{width: '30%'}}>
           <Button title={'Back'} onPress={() => navigation.navigate('Dashboard')}/>
@@ -71,4 +76,19 @@ export default function Todo({route, navigation}) {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  textWrapper: {
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  }
+})
