ee60c7de2835c738f722f3272eb2412608415c1f
Hamzah Nur Rohman <50015789+hamzahnr7@users.noreply.github.com>
2023-06-07 22:06:33 +0700

    update env

--- src/Env.js
+++ src/Env.js
@@ -1,4 +1,4 @@
-const domain = 'https://2743-125-160-157-176.ngrok-free.app';
+const domain = 'https://d4cb-103-233-100-228.ngrok-free.app';
 // const domain = 'http://localhost:3333';
 const url = `${domain}/todo`;
 
